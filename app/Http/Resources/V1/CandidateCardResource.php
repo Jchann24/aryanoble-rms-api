4a0e024090b2b00d626f24c55cdb3c73f8bb966c
Jason  Chan <jasonnchann24@gmail.com>
2020-07-17 15:09:28 +0700

    simplified table for pic erf

--- app/Http/Resources/V1/CandidateCardResource.php
+++ app/Http/Resources/V1/CandidateCardResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\V1;
 
+use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CandidateCardResource extends JsonResource
@@ -14,6 +15,16 @@ class CandidateCardResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'pic' => new UserResource($this->pic),
+            'candidate' => new UserResource($this->candidate),
+            'created_at' => $this->created_at,
+            'erf' => new ErfResource($this->erf),
+            'interview_detail' => new InterviewDetailResource($this->interviewDetail),
+            'last_updated_by' => $this->lastUpdatedBy->name,
+            'status' => $this->status,
+            'talent' => new TalentResource($this->talent)
+        ];
     }
 }
